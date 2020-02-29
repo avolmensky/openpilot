aff85aea8b7711e6d0dbe41212ba5b277928378a
Andre Volmensky <andre@volmens.ky>
2020-02-29 09:20:02 +0900

    Changes for PR

--- selfdrive/car/nissan/carstate.py
+++ selfdrive/car/nissan/carstate.py
@@ -55,6 +55,7 @@ class CarState(CarStateBase):
     ret.steeringTorque = cp.vl["Steering"]["DriverTorque"]
     ret.steeringAngle = cp.vl["SteeringWheel"]["Steering_Angle"]
 
+    self.angle_steers = cp.vl["SteeringWheel"]["Steering_Angle"]
     self.acc_active = bool(cp_cam.vl["ProPilot"]["CRUISE_ACTIVATED"])
     self.cruise_throttle_msg = copy.copy(cp.vl["CruiseThrottle"])
 
